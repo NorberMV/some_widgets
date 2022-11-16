30cb4a383f8daf72ea0464ee1e4cb385b7430c13
Norber MV <norbermv@Norbertos-MacBook-Air.local>
2022-11-15 22:05:31 -0500

    Compile resources.

---
+++ todo_list_demo/todo_app.py
@@ -0,0 +1,7 @@
+
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap, QImage
+from PySide6.QtWidgets import QMainWindow, QApplication
+
+from ui.todomainwindow import Ui_MainWindow
