a0e180ad28ebe4422828804187af3750b7e1f9a5
Norber MV <norbermv@Norbertos-MacBook-Air.local>
2022-11-16 21:52:30 -0500

    Create a MainWindow class to trigger the setup of the app UI

--- todo_list_demo/todo_app.py
+++ todo_list_demo/todo_app.py
@@ -1,7 +1,27 @@
-
+import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap, QImage
 from PySide6.QtWidgets import QMainWindow, QApplication
 
-from ui.todomainwindow import Ui_MainWindow
+from ui.todoMainWindow import Ui_MainWindow
+from resources import resources_rc
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    """
+    Basic class to build the app UI.
+    """
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+
+
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    app.setStyle("Fusion")
+    app.exec()
